9c411d39213bd6f05e9bacfad3793b022f916688
Kirill Marinushkin <k.marinushkin@gmail.com>
2022-09-10 21:41:11 +0200

    base_providers: Add method `_get_target_scopes`
    This method create a list of scopes, for which targets exist.
    Needed for future steps, to calculate temperature score for
    all target scopes.
    
    Signed-off-by: Kirill Marinushkin <k.marinushkin@gmail.com>

--- ITR/interfaces.py
+++ ITR/interfaces.py
@@ -173,7 +173,7 @@ class EScope(SortableEnum):
 
         :return: A list of EScope objects
         """
-        return [cls.S1S2, cls.S3, cls.S1S2S3]
+        return [cls.S1, cls.S2, cls.S1S2, cls.S3, cls.S1S2S3]
 
 
 class ETimeFrames(SortableEnum):
