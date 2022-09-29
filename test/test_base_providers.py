6cf783a37645d4c498d18b71d91ac981010520aa
Kirill Marinushkin <k.marinushkin@gmail.com>
2022-09-29 20:36:29 +0200

    base_providers: Production benchmarks are relevant to all scopes
    Modify production benchmark input to `AllScopes`
    
    Signed-off-by: Kirill Marinushkin <k.marinushkin@gmail.com>

--- test/test_base_providers.py
+++ test/test_base_providers.py
@@ -201,6 +201,10 @@ class TestBaseProvider(unittest.TestCase):
         # Verify S3 is NOT folded into S1S2
         self.assertNotEqual(company_with_s3.ghg_s3, 0)
 
+    def test_production_benchmark_all_scopes(self):
+        pbm = self.base_production_bm._productions_benchmarks
+        self.assertEqual(len(pbm.AllScopes.benchmarks), 42)
+
 
 if __name__ == "__main__":
     test = TestBaseProvider()
